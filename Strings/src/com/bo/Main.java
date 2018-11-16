3f28414d56be122f3e878a490b83177eab0c3962
boyang1020 <boyang1020@gmail.com>
2018-11-15 19:15:34 -0800

    String Continued

--- Strings/src/com/bo/Main.java
+++ Strings/src/com/bo/Main.java
@@ -17,5 +17,21 @@ public class Main {
         myString = myString + ", and this is more.";
         System.out.println("myString is equal to " + myString);
 
+        myString = myString + "\u00A9 2018";
+
+        System.out.println("myString is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+
+        System.out.println("The result is " + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+
+        lastString = lastString + myInt;
+
+        System.out.println("LastString is equal to " + lastString);
+
     }
 }
