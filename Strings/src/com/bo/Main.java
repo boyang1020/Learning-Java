52ec7978167b51b654269ea4800642f136ab9d5f
boyang1020 <boyang1020@gmail.com>
2018-11-15 19:05:37 -0800

    String

--- Strings/src/com/bo/Main.java
+++ Strings/src/com/bo/Main.java
@@ -3,6 +3,19 @@ package com.bo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// byte
+    // short
+    // int
+    // long
+    // float
+    // double
+    // char
+    // boolean
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+
     }
 }
