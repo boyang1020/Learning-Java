d43cf59e8fd13d828f3f92103a96deee2b007bb8
boyang1020 <boyang1020@gmail.com>
2018-11-15 19:27:53 -0800

    Operators

---
+++ Operators/src/com/bo/Main.java
@@ -0,0 +1,42 @@
+package com.bo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+
+        result = result - 1;
+
+        System.out.println(previousResult + " - 1 = " + result);
+
+        previousResult = result;
+
+        result = result * 10;
+        System.out.println(previousResult + " * 10 = " + result);
+
+        previousResult = result;
+
+        result = result /5;
+        System.out.println(previousResult + " / 5 = " + result);
+
+        previousResult = result;
+        result = result % 3;
+
+        System.out.println(previousResult + " % 3 = " + result);
+
+        previousResult = result;
+        result++;
+
+        System.out.println("result now = " + result);
+
+        result += 2;
+
+        System.out.println(" result now = " + result);
+
+
+    }
+}
