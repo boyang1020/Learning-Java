d69363406084168e497188b4f62ee876d892c0ef
boyang1020 <boyang1020@gmail.com>
2018-11-19 23:19:09 -0800

    Java keywords and expressions

---
+++ KeywordsAndExpressions/src/com/bo/Main.java
@@ -0,0 +1,19 @@
+package com.bo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//  Java reserved keywords such as false, true, null
+
+//        a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        System.out.println("kilometers = " + kilometers  );
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+    }
+}
+
