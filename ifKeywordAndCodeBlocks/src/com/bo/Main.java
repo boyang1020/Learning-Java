563ec887e14ecd63f4033e018700191d3e41702c
boyang1020 <boyang1020@gmail.com>
2018-11-19 23:39:37 -0800

    code blocks

---
+++ ifKeywordAndCodeBlocks/src/com/bo/Main.java
@@ -0,0 +1,19 @@
+package com.bo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(score == 4000) {
+            System.out.println("your score was 5000");
+            System.out.println("This was executed");
+        }
+
+//       above if statement is a code block
+    }
+}
