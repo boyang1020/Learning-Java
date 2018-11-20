2ba365928f3ff05d4927a64f01e74e1f90484855
boyang1020 <boyang1020@gmail.com>
2018-11-19 23:25:22 -0800

    Java statements

---
+++ StatementsWhiteSpaceAndIndenting/src/com/bo/Main.java
@@ -0,0 +1,14 @@
+package com.bo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int myVariable = 50;
+	 myVariable ++;
+	 myVariable --;
+//	  above lines are complete Java statements
+//        This line below is also a statement
+        System.out.println("this is a test");
+//        semicolon is needed to complete a Java line, to make a statement
+    }
+}
