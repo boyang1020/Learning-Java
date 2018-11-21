4396a6335e196e5af1386f9d67a541712cd36742
boyang1020 <boyang1020@gmail.com>
2018-11-20 19:28:22 -0800

    definition of method

---
+++ Methods/src/com/bo/Main.java
@@ -0,0 +1,13 @@
+package com.bo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        any variable and code executed inside this, and this is called methods
+    }
+
+    public static void calculateScore() {
+//        this is a calculate score method
+    }
+}
