bdcbd3a00c3c6c4f21d482c89ae55d338e08ea06
boyang1020 <boyang1020@gmail.com>
2018-11-27 21:08:41 -0800

    refactor

--- Method2/src/com/bo/Main.java
+++ Method2/src/com/bo/Main.java
@@ -34,3 +34,14 @@ public class Main {
         }
     }
 }
+
+
+// int position = 4;
+//if(playerScore >= 1000) {
+//    position = 1;
+//        } else if(playerScore >= 500) {
+//    position = 2;
+//        } else if(playerScore >= 100;) {
+//    position = 3;
+//        }
+//        return position;
