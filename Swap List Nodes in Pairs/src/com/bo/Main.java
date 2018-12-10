50ee12f5b45283ce3ed8e6e5ff888b1a587c945e
boyang1020 <boyang1020@gmail.com>
2018-12-09 19:00:28 -0800

    solution two

--- Swap List Nodes in Pairs/src/com/bo/Main.java
+++ Swap List Nodes in Pairs/src/com/bo/Main.java
@@ -31,6 +31,41 @@ public class Main {
         }
         return dummy.next;
     }
+
+
+    /*
+
+    solution two
+
+     public ListNode swapPairs(ListNode head) {
+    if(head==null || head.next==null)
+        return head;
+
+    //a fake head
+    ListNode h = new ListNode(0);
+    h.next = head;
+
+    ListNode p1 = head;
+    ListNode p2 = head.next;
+
+    ListNode pre = h;
+    while(p1!=null && p2!=null){
+        pre.next = p2;
+
+        ListNode t = p2.next;
+        p2.next = p1;
+        pre = p1;
+        p1.next = t;
+
+        p1 = p1.next;
+
+        if(t!=null)
+            p2 = t.next;
+    }
+
+    return h.next;
+}
+*/
     /******************************结束写代码******************************/
 
 
