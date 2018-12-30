d7fa9ee96a90fa5d5318fd15d4955dc6d3ca4f8b
boyang1020 <boyang1020@gmail.com>
2018-12-30 14:46:08 -0800

    find largest value in each tree row

---
+++ find largest value in each tree row/src/com/bo/Main.java
@@ -0,0 +1,116 @@
+/*
+You need to find the largest value in each row of a binary tree.
+
+ */
+
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+
+    ************************************************************/
+    static List<Integer> largestValues(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Deque<TreeNode> queue = new LinkedList<>();
+        if(root != null) queue.offer(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            while(size > 0) {
+                TreeNode node = queue.poll();
+                max = Math.max(max, node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+                size--;
+            }
+            list.add(max);
+        }
+        return list;
+    }
+
+    /************************************************************/
+
+    static int preIndex = 0;
+    private static TreeNode buildTree(int in[], int pre[], int inStrt, int inEnd) {
+        if (inStrt > inEnd)
+            return null;
+
+        /* Pick current node from Preorder traversal using preIndex
+           and increment preIndex */
+        TreeNode tNode = new TreeNode(pre[preIndex++]);
+
+        /* If this node has no children then return */
+        if (inStrt == inEnd)
+            return tNode;
+
+        /* Else find the index of this node in Inorder traversal */
+        int inIndex = search(in, inStrt, inEnd, tNode.val);
+
+        /* Using index in Inorder traversal, construct left and
+           right subtress */
+        tNode.left = buildTree(in, pre, inStrt, inIndex - 1);
+        tNode.right = buildTree(in, pre, inIndex + 1, inEnd);
+
+        return tNode;
+    }
+
+    /* UTILITY FUNCTIONS */
+
+    /* Function to find index of value in arr[start...end]
+     The function assumes that value is present in in[] */
+    private static int search(int arr[], int strt, int end, int value)
+    {
+        int i;
+        for (i = strt; i <= end; i++)
+        {
+            if (arr[i] == value)
+                return i;
+        }
+        return i;
+    }
+
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        List<Integer> res;
+
+        int _root_rows = 0;
+        int _root_cols = 0;
+        _root_rows = Integer.parseInt(in.nextLine().trim());
+        _root_cols = Integer.parseInt(in.nextLine().trim());
+
+        int[][] _root = new int[_root_rows][_root_cols];
+        for(int _root_i=0; _root_i<_root_rows; _root_i++) {
+            for(int _root_j=0; _root_j<_root_cols; _root_j++) {
+                _root[_root_i][_root_j] = in.nextInt();
+
+            }
+        }
+
+        if(in.hasNextLine()) {
+            in.nextLine();
+        }
+
+        TreeNode root = buildTree(_root[0], _root[1], 0, _root_cols - 1);
+        res = largestValues(root);
+        StringBuilder s = new StringBuilder();
+        for(int res_i=0; res_i < res.size(); res_i++) {
+            s.append(res.get(res_i) + " ");
+        }
+        System.out.println(s.toString());
+    }
+}
